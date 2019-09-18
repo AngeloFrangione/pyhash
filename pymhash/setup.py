2a196baa275c04546a51a7002f00bb5b4f4266f2
angelo <Angelo Frangione>
2019-09-18 08:37:40 +0000

    named fixed

--- pymhash/setup.py
+++ pymhash/setup.py
@@ -8,7 +8,7 @@ hasher = Extension('hasher',
                     libraries=['hasher'])
 
 
-setup(name='pypmhash',
+setup(name='pymhash',
       version = '0.0.1',
       description = 'Python interface to a C++ library to calculate multiple MD5 hashes at same time.',
       author = 'Angelo Frangione',
